6207ab39b74543fe7b6fae0941499e46fb3aea60
Angel Rivero <angelantonio3000@gmail.com>
2019-03-31 21:05:50 -0400

    Initial Commit

---
+++ PlayingCard/CardBehavior.swift
@@ -0,0 +1,71 @@
+//
+//  CardBehavior.swift
+//  PlayingCard
+//
+//  Created by Romelys Rivero on 3/3/19.
+//  Copyright © 2019 Angel Rivero. All rights reserved.
+//
+
+import UIKit
+
+class CardBehavior: UIDynamicBehavior {
+    
+    lazy var collisionBehavior : UICollisionBehavior = {
+        let behavior = UICollisionBehavior()
+        behavior.translatesReferenceBoundsIntoBoundary = true
+        return behavior
+    }()
+    
+    lazy var itemBehavior: UIDynamicItemBehavior = {
+        let behavior = UIDynamicItemBehavior()
+        behavior.allowsRotation = false
+        behavior.elasticity = 1.0
+        behavior.resistance = 0
+        return behavior
+    }()
+    
+    private func push(_ item: UIDynamicItem) {
+        let push = UIPushBehavior(items: [item], mode: .instantaneous)
+        if let referenceBounds = dynamicAnimator?.referenceView?.bounds {
+            let center = CGPoint(x: referenceBounds.midX, y: referenceBounds.midY)
+            push.angle = (CGFloat.pi/2).arc4random
+            switch (item.center.x, item.center.y) {
+            case let (x, y) where x < center.x && y > center.y:
+                push.angle = -1 * push.angle
+            case let (x, y) where x > center.x:
+                push.angle = y < center.y ? CGFloat.pi-push.angle: CGFloat.pi+push.angle
+            default:
+                push.angle = (CGFloat.pi*2).arc4random
+            }
+        }
+        //push.magnitude = CGFloat(Constants.behaviorPushMagnitudeMinimum) + CGFloat(Constants.behaviorPushMagnitudeRandomFactor).arc4random
+        push.magnitude = CGFloat(1.0) + CGFloat(2.0).arc4random
+        push.action = { [unowned push, weak self] in
+            self?.removeChildBehavior(push)
+        }
+        addChildBehavior(push)
+    }
+    
+    func addItem(_ item: UIDynamicItem){
+        collisionBehavior.addItem(item)
+        itemBehavior.addItem(item)
+        push(item)
+    }
+    
+    func removeItem(_ item: UIDynamicItem){
+        collisionBehavior.removeItem(item)
+        itemBehavior.removeItem(item)
+    }
+    
+    override init () {
+        super.init()
+        addChildBehavior(collisionBehavior)
+        addChildBehavior(itemBehavior)
+    }
+    
+    convenience init(in animator: UIDynamicAnimator) {
+        self.init()
+        animator.addBehavior(self)
+    }
+    
+}
